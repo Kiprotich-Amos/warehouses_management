35ce1efb601582ef00d565246ece43cc6ae2df44
Kiprotich-Amos <kiprotichamoh11@gmail.com>
2025-03-25 23:54:03 +0300

    home

---
+++ warehouse_management/warehouse_management/doctype/customer_group/customer_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Amos kiprotich Rono and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Customer Group", {
+// 	refresh(frm) {
+
+// 	},
+// });
